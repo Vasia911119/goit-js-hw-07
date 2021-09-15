290d983e736846d059b1bd9a835ed53c30fe501a
Vasyl Yakymets <Vasia911119@gmail.com>
2021-09-15 18:42:57 +0300

    Resolved task-01

--- js/task-01.js
+++ js/task-01.js
@@ -1,1 +1,23 @@
+//Print the number of categories
 
+const listOfCategories = document.querySelector("#categories").children;
+console.log(`В списке ${listOfCategories.length} категории.`);
+
+//Display the title and number of items in the category
+
+for (let i = 0; i < listOfCategories.length; i++) {
+    const elem = listOfCategories[i];
+
+    console.log(`Категория: ${elem.querySelector("h2").textContent}`);
+    console.log(`Количество элементов: ${elem.querySelector("ul").children.length}`);
+}
+
+//Variant 2
+
+// const listOfCategories = document.querySelector("#categories").children;
+// console.log(`В списке ${listOfCategories.length} категории.`);
+
+// [...listOfCategories].forEach((elem) => {
+//     console.log(`Категория: ${elem.querySelector("h2").textContent}`);
+//     console.log(`Количество элементов: ${elem.querySelector("ul").children.length}`);
+// });
